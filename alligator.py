f6dfbdab75737a6e0386acd155f57652338d4b8b
anandramm <53943521+anandramm@users.noreply.github.com>
2020-07-13 17:10:37 +0530

    Add files via upload
    To Rename a File with current date

---
+++ alligator.py
@@ -0,0 +1,7 @@
+import os
+from datetime import date
+
+b=str(date.today())
+src = 'C:\\Users\\Anandram Mariappan\\Desktop\\rig.txt'
+dst = 'C:\\Users\\Anandram Mariappan\\Desktop\\rig_'+b+'.txt'
+os.rename(src, dst)
